fc55a0a1bf27d495fbd4690612abbcea9a3dbb3a
mercy-5120 <mgashau18@gmail.com>
2025-04-29 08:59:07 +0300

    Coding Challenge 5

---
+++ src/pkg_OOPCodingChallenge5/Main.java
@@ -0,0 +1,9 @@
+package pkg_OOPCodingChallenge5;
+
+public class Main {
+
+        public static void main (String[]args) {
+            HelloTillStop stop = new HelloTillStop();
+            stop.showHello();
+        }
+}
